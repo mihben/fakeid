673554a727ffd99ab2eb2543eda0c744e2bacc97
Mihucza, Bence <mihben@gmail.com>
2025-11-27 11:48:27 +0100

    chore: Created projects
    Added:
    - FakeID.Api
    - FakeID.Application
    - FakeID.Host
    - FakeID.Application.Test.Unit
    - FakeID.Client.Blazor
    - FakeID.Client.Blazor.Test.Unit

---
+++ src/FakeID.Client.Blazor/Program.cs
@@ -0,0 +1,11 @@
+using FakeID.Client.Blazor;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
