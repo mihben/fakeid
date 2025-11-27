30865a8dd2406b915587b00ecf422b990e79293e
Mihucza, Bence <mihben@gmail.com>
2025-11-27 12:50:24 +0100

    chore: Wireup STrain to Blazor

--- src/FakeID.Client.Blazor/Program.cs
+++ src/FakeID.Client.Blazor/Program.cs
@@ -1,11 +1,16 @@
 using FakeID.Client.Blazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using STrain;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient();
+builder.UseLightinject();
+
+builder.UseRequestRouter(_ => "backend")
+    .AddGenericHttpSender("backend", (options, _) => { options.BaseAddress = new Uri("http://localhost:5100/"); options.Path = "api"; });
 
 await builder.Build().RunAsync();
