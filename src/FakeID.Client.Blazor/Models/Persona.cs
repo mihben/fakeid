7eaffbf3c8689ca57130f29b2d30a68e4ecc1d04
Mihucza, Bence <mihben@gmail.com>
2025-12-10 23:59:00 +0100

    feat: Added editcontext to persona editor

--- src/FakeID.Client.Blazor/Models/Persona.cs
+++ src/FakeID.Client.Blazor/Models/Persona.cs
@@ -1,11 +1,17 @@
-﻿namespace FakeID.Client.Blazor.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FakeID.Client.Blazor.Models
 {
     public class Persona
     {
         public Guid Id { get; init; }
+        [Required]
         public string? FirstName { get; set; }
+        [Required]
         public string? LastName { get; set; }
+        [Required]
         public string? Email { get; set; }
+        [Required]
         public string? Role { get; set; }
     }
 }
